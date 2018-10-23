85d8b4bc8007987482fb8212b2a98e5219eae6f8
Palfavi Lorand <lorand.palfalvi@yopeso.com>
2018-10-23 23:21:48 +0300

    Added hello world

--- src/App.js
+++ src/App.js
@@ -1,64 +1,20 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet, Text, View, Platform, Image,
+  StyleSheet, Text, View,
 } from 'react-native';
-import {
-  Segment, Button, Header,
-} from 'native-base';
 
-type Props = {};
-
-const cat1 = require('./../assets/cat1.jpeg');
-const cat2 = require('./../assets/cat2.jpg');
-
-export default class App extends Component<Props> {
+export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      imageSource: null,
     };
   }
 
-  renderImage = (source) => {
-    this.setState({
-      imageSource: source,
-    });
-  }
-
   render() {
-    const { imageSource } = this.state;
     return (
       <View style={styles.container}>
-        <Header
-          hasSegment
-        />
-        <Segment>
-          <Button
-            first
-            style={styles.button}
-            onPress={() => this.renderImage(cat1)}
-          >
-            <Text style={[styles.text, Platform.OS === 'android' ? { color: 'white' } : {}]}>Drama</Text>
-          </Button>
-          <Button
-            last
-            style={styles.button}
-            onPress={() => this.renderImage(cat2)
-            }
-          >
-            <Text style={[styles.text, Platform.OS === 'android' ? { color: 'white' } : {}]}>Comedy</Text>
-          </Button>
-        </Segment>
-        {
-          imageSource ? (
-            <Image
-              style={styles.image}
-              source={imageSource}
-            />
-          )
-            : null
-        }
+        <Text style={styles.text}>Hello World!</Text>
       </View>
     );
   }
@@ -67,16 +23,10 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     fontSize: 18,
-  },
-  button: {
-    padding: 10,
-  },
-  image: {
-    flex: 1,
-    width: null,
-    resizeMode: 'contain',
   },
 });
